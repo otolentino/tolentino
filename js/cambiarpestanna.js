4c5acb7cafad079af668b1e185f20058d2562a6f
oscar <otolentino@tol.com>
2021-10-26 12:25:51 -0500

    first commit

--- js/cambiarpestanna.js
+++ js/cambiarpestanna.js
@@ -1,16 +1,17 @@
-quiere mostrar, la funcion oculta todas las pestañas a excepcion de esa.
+// Dadas la division que contiene todas las pestañas y la de la pestaña que se
+// quiere mostrar, la funcion oculta todas las pestañas a excepcion de esa.
 function cambiarPestanna(pestannas,pestanna) {
-    
+
     // Obtiene los elementos con los identificadores pasados.
     pestanna = document.getElementById(pestanna.id);
     listaPestannas = document.getElementById(pestannas.id);
-    
+
     // Obtiene las divisiones que tienen el contenido de las pestañas.
     cpestanna = document.getElementById('c'+pestanna.id);
     listacPestannas = document.getElementById('contenido'+pestannas.id);
-    
+
     i=0;
-    // Recorre la lista ocultando todas las pestañas y restaurando el fondo 
+    // Recorre la lista ocultando todas las pestañas y restaurando el fondo
     // y el padding de las pestañas.
     while (typeof listacPestannas.getElementsByTagName('div')[i] != 'undefined'){
         $(document).ready(function(){
@@ -23,12 +24,12 @@ function cambiarPestanna(pestannas,pestanna) {
 
     $(document).ready(function(){
         // Muestra el contenido de la pestaña pasada como parametro a la funcion,
-        // cambia el color de la pestaña y aumenta el padding para que tape el  
-        // borde superior del contenido que esta juesto debajo y se vea de este 
+        // cambia el color de la pestaña y aumenta el padding para que tape el
+        // borde superior del contenido que esta juesto debajo y se vea de este
         // modo que esta seleccionada.
         $(cpestanna).css('display','');
         $(pestanna).css('background','dimgray');
-        $(pestanna).css('padding-bottom','2px'); 
+        $(pestanna).css('padding-bottom','2px');
     });
 
 }
